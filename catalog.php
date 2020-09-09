749c71b33c5a76c2fd233301d88c6c96f19db2e8
pivannikov <pivannikov@yandex.ru>
2020-09-09 10:52:20 +0200

    start

---
+++ catalog.php
@@ -0,0 +1,11 @@
+<?php
+
+$arr = ['a' => 'red', 'b' => 'blue', 'o' => 'orange'];
+
+echo json_encode($arr);
+$goods[] = $arr;
+
+
+
+echo json_encode($goods);
+print_r($goods);
---
+++ main.js
@@ -0,0 +1,30 @@
+var art = new Vue({
+    el: '#art',
+    data: {
+        articles: [
+            {
+                author: 'London',
+                date: '20-04-2020',
+                title: 'When is the Spring season? ',
+                desc: 'Spring is the season succeeding Winter and preceding Summer.',
+                text: 'Spring is the season succeeding Winter and preceding Summer. Spring refers to the season as well as to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth. During spring an important celebration takes place: Easter Day. It varies between March 22 and April 25 in Western tradition, and between April 4 and May 8 in Eastern Christianity.'
+            },
+            {
+                author: 'London',
+                date: '20-04-2020',
+                title: 'Lets go',
+                desc: 'Spring is the season succeeding Winter and preceding Summer.',
+                text: 'Spring is the season succeeding Winter and preceding Summer. Spring refers to the season as well as to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth. During spring an important celebration takes place: Easter Day. It varies between March 22 and April 25 in Western tradition, and between April 4 and May 8 in Eastern Christianity.'
+            },
+            {
+                author: 'London',
+                date: '20-04-2020',
+                title: 'Lets go',
+                desc: 'Spring is the season succeeding Winter and preceding Summer.',
+                text: 'Spring is the season succeeding Winter and preceding Summer. Spring refers to the season as well as to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth. During spring an important celebration takes place: Easter Day. It varies between March 22 and April 25 in Western tradition, and between April 4 and May 8 in Eastern Christianity.'
+            },
+        ]
+    }
+})
+
+
